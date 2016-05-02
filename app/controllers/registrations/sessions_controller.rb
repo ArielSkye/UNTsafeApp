5aab9bdb31d6c0945b540ca9b3e68954634c094a
Ariel Bailey <arielskyebailey@gmail.com>
2016-05-02 10:53:08 -0500

    edit sessions controller for Heroku 2

--- app/controllers/registrations/sessions_controller.rb
+++ app/controllers/registrations/sessions_controller.rb
@@ -1,10 +1,10 @@
-class registrations::sessionsController < Devise::sessionsController
+class Registrations::SessionsController < Devise::SessionsController
 
 	@users = User.all
 @hash = Gmaps4rails.build_markers(@users) do |user, marker|
   marker.lat user.latitude
   marker.lng user.longitude
-end
+	end
 
   # def new
   #   super
